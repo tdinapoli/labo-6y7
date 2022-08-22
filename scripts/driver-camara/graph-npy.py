703391f0e1600dd8d2a2a7e81e6c58003c43a1ab
soydina <tomas.dina98@gmail.com>
2022-08-22 09:50:24 -0300

    notas presentacion e informe

--- scripts/driver-camara/graph-npy.py
+++ scripts/driver-camara/graph-npy.py
@@ -1,16 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import correlate2d
+import imageio
 
 def normalizar(img):
     maximo = np.max(img)
     img = img/maximo
     return img
 
-img = np.load("/home/chanoscopio/fpm_samples/22_junio/16_16_g_26746.npy")
-#img = np.load("matricesMVLD/m_ML.npy")
+#img = np.load("/home/chanoscopio/fpm_samples/22_junio/16_16_g_26746.npy")
+img = np.load("scripts/epi-iluminacion/comparacion-anillo-transmision.npy")[:, 2000:7000]
+kernel = imageio.imread("~/fpm_samples/segunda_rec/Reconstruction/b/magnitude/07_18.tif")
+#img = np.load("matricesMVLD/b_MD.npy")
 print(img)
 #plt.imshow(img, origin="lower", interpolation="none", vmin=np.min(img), vmax=np.max(img))
-plt.imshow(img, cmap = 'gray', vmin=np.min(img), vmax=np.max(img))
+#plt.imshow(img, cmap = 'gray', vmin=np.min(img), vmax=np.max(img))
+#plt.show()
+
+cor = correlate2d(img, kernel)
+
+fig, axs = plt.subplots(1,3)
+ax1, ax2, ax3 = axs
+ax1.imshow(kernel)
+ax2.imshow(img)
+ax3.imshow(cor)
 plt.show()
 
-print(np.max(img), np.min(img), np.mean(img))
+print(np.max(img), np.min(img), np.mean(img), np.std(img))
--- scripts/driver-camara/histograma.py
+++ scripts/driver-camara/histograma.py
@@ -1,9 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-im = np.load("oscuridad_black_level.npy")
+im = np.load("matricesMVLD/dark/mean_1000000.npy")
 mx, mi = im.max(), im.min()
+mx, mi = round(mx), round(mi)
 hist, bin_edges = np.histogram(im, bins=mx- mi, range=(mi,mx), density=False)
 
-plt.plot(bin_edges[:-1], hist, 'ok')
+#plt.plot(bin_edges[:-1], hist, 'ok')
+
+im = np.load("matricesMVLD/dark/mean_15.npy")
+mx, mi = im.max(), im.min()
+mx, mi = round(mx), round(mi)
+hist, bin_edges = np.histogram(im, bins=mx- mi, range=(mi,mx), density=False)
+
+plt.hist(hist, bins = bin_edges)
+
+plt.xlim([150,200])
 plt.show()
