70c6866ae4003a95e8112c366e5c229d9b1cc4ba
SoyDina <tomas.dina98@gmail.com>
2022-08-26 00:55:02 -0300

    saco funciones que no hacen nada y agrego código para los gráficos

--- scripts/sph-illumination/parametrization.py
+++ scripts/sph-illumination/parametrization.py
@@ -9,14 +9,6 @@ def calculate_position_sph(z0, R, tita_max, fi0, n_leds, m, n): # units in mm
     z = -z0 + R*np.cos(titan)
     return (x, y, z)
 
-def calculate_positions_sph(*args):
-    max_steps = int(2*np.pi/fi0)
-    positions = []
-    for m in range(0, max_steps, n_steps):
-        for n in range(1, n_leds + 1):
-            positions.append(calculate_position_sph(z0, R, tita_max, fi0, n_leds, m, n))
-    return positions
-            
 
 def calculate_k_vector_general(led_position, wavelength):
     x, y, z = led_position
@@ -29,15 +21,6 @@ def calculate_k_vector_general(led_position, wavelength):
     k_vector = np.array([kx,ky,kz])*k_0
     return k_vector
 
-def calculate_k_vector_sph(z0, R, tita_max, fi0, n_leds, wavelength,m ,n):
-    x, y, z = calculate_position_sph(z0, R, tita_max, fi0, n_leds, m, n)
-    k_0 = 2*np.pi/wavelength
-    module = np.linalg.norm(np.array([x,y,z]))
-    kx = -x/module
-    ky = -y/module
-    kz = -z/module
-    k_vector = np.array([kx,ky,kz])*k_0
-    return k_vector
 
 def calculate_k_vectors_sph(z0, R, tita_max, fi0, n_leds, n_steps, wavelength):
     k_vectors = []
@@ -56,16 +39,6 @@ def calculate_position_matrix(z0, dx, dy, i, j, led_0_0_offset=(0,0)):
     x, y = j*dx + led_0_0_offset[0], i*dy + led_0_0_offset[1]
     z = -z0
     return (x, y, z)
-
-def calculate_k_vector_matrix(z0, dx, dy, wavelength, i, j, led_0_0_offset=(0,0)):
-    x, y, z = calculate_position_matrix(z0, dx, dy, i, j, led_0_0_offset=led_0_0_offset)
-    k_0 = 2*np.pi/wavelength
-    module = np.linalg.norm(np.array([x,y,z]))
-    kx = -x/module
-    ky = -y/module
-    kz = -z/module
-    k_vector = np.array([kx, ky, kz])*k_0
-    return k_vector
 
 def calculate_k_vectors_matrix(z0, dx, dy, wavelength, x_range=range(-2, 3, 1), y_range=range(-2, 3, 1) ,led_0_0_offset=(0,0)):
     k_vectors = []
@@ -113,7 +86,8 @@ def overlap(k_vectors, NA, freq_per_pix=1.6e4, shape=(1000,1000)):
         kx, ky, kz = k_vector
         msk = create_circular_mask(shapex, shapey, center=(kx, ky), radius=radius)
         img[msk] = img[msk] + 1
-    ax.imshow(img)
+    pos = ax.imshow(img)
+    fig.colorbar(pos, ax=ax)
     ax.set_title("wavelength="+str(wavelength)+"m\nfreq_per_pix="+str(freq_per_pix)+"$m^{-1}$"+"\n n_fotos="+str(len(k_vectors)))
     return img
 
@@ -139,7 +113,7 @@ def graph_led_positions_sph(k_vectors, z0, R, tita_max, fi0, n_leds, n_steps, ax
             x, y, z = calculate_position_sph(z0, R, tita_max, fi0, n_leds, m, n)
             ax.scatter(x, y, z, color="tab:brown")
             kx, ky, kz = vector_length* k_vectors[i+1]/k0
-            ax.quiver(x,y,z,kx,ky,kz, linewidth=1,color="tab:olive")
+            #ax.quiver(x,y,z,kx,ky,kz, linewidth=1,color="tab:olive")
             i = i+1
 
 
@@ -151,15 +125,15 @@ def graph_led_positions_matrix(k_vectors, z0, dx, dy, ax, range_x=range(-2, 3, 1
             x, y, z = calculate_position_matrix(z0, dx, dy, i, j, led_0_0_offset=(0,0))
             ax.scatter(x,y,z, color="tab:orange")
             kx, ky, kz = vector_length * k_vectors[ii]/k_0
-            ax.quiver(x, y, z, kx, ky, kz, linewidth=1,color="tab:blue")
+            #ax.quiver(x, y, z, kx, ky, kz, linewidth=1,color="tab:blue")
             ii = ii + 1
 
 
         
 z0 = 0
 R = 100
-tita_max = np.pi/4
-fi0 = 2*np.pi/30
+tita_max = np.pi/3
+fi0 = 2*np.pi/60
 n_leds = 6
 wavelength = 500e-9
 NA = 0.1
@@ -175,29 +149,29 @@ vector_length=10
 freq_per_pix=4e3
 
 k_vectors_sph = calculate_k_vectors_sph(z0, R, tita_max, fi0, n_leds, n_steps, wavelength)
+k_vectors_matrix = calculate_k_vectors_matrix(z0_matrix, dx, dy, wavelength, x_range=range_x, y_range=range_y) 
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.set_xlim([-100, 100])
+ax.set_ylim([-100, 100])
+ax.set_zlim([-250, -50])
+ax.scatter(0,0,0, color="k", s=30)
+
+graph_led_positions_sph(k_vectors_sph, z0, R, tita_max, fi0, n_leds, n_steps, ax)
+#plt.show()
 
 #fig = plt.figure()
 #ax = fig.add_subplot(projection='3d')
 #ax.set_xlim([-100, 100])
 #ax.set_ylim([-100, 100])
 #ax.set_zlim([-250, -50])
-#ax.scatter(0,0,0, color="k", s=30)
-#
-#graph_led_positions_sph(k_vectors_sph, z0, R, tita_max, fi0, n_leds, n_steps, ax)
-#plt.show()
-#
-#fig = plt.figure()
-#ax = fig.add_subplot(projection='3d')
-#ax.set_xlim([-100, 100])
-#ax.set_ylim([-100, 100])
-#ax.set_zlim([-250, -50])
-#
-k_vectors_matrix = calculate_k_vectors_matrix(z0_matrix, dx, dy, wavelength, x_range=range_x, y_range=range_y) 
-#
-#graph_led_positions_matrix(k_vectors_matrix, z0_matrix, dx, dy, ax, range_x = range_x, range_y=range_y)
-#
-#plt.show()
-#
+
+
+graph_led_positions_matrix(k_vectors_matrix, z0_matrix, dx, dy, ax, range_x = range_x, range_y=range_y)
+
+plt.show()
+
 #
 #graph_support(k_vectors_sph, NA)
 #graph_support(k_vectors_matrix, NA)
@@ -205,8 +179,47 @@ k_vectors_matrix = calculate_k_vectors_matrix(z0_matrix, dx, dy, wavelength, x_r
 
 #erres = np.arange()
 n_samples = 1000
-overlap(k_vectors_sph, NA, freq_per_pix=freq_per_pix, shape=(1000,1000))
-overlap(k_vectors_matrix, NA, freq_per_pix=freq_per_pix, shape=(1000,1000))
+ol_sph = overlap(k_vectors_sph, NA, freq_per_pix=freq_per_pix, shape=(1000,1000))
+ol_mat = overlap(k_vectors_matrix, NA, freq_per_pix=freq_per_pix, shape=(1000,1000))
 plt.show()
+
+
+n_pix = n_samples**2
+n_nonzero_sph = np.count_nonzero(ol_sph > 0)
+n_nonzero_mat = np.count_nonzero(ol_mat > 0)
+nums_ol = np.arange(1, 21, 1)
+ols_sph = []
+ols_mat = []
+for num_ol in nums_ol:
+    ols_sph.append(np.count_nonzero(ol_sph >= num_ol)/n_pix)
+    ols_mat.append(np.count_nonzero(ol_mat >= num_ol)/n_pix)
+
+bin_bound = np.arange(1, 21, 1)
+
+plt.plot(nums_ol, ols_sph, 'o', label="Esférica")
+plt.plot(nums_ol, ols_mat, 'o', label="Matriz")
+plt.legend()
+plt.xticks(bin_bound)
+plt.xlabel("n_circulos", fontsize=15)
+plt.ylabel("Proporcion de pixeles", fontsize=15)
+plt.title("proporcion de pixeles cubiertos por al menos n_circulos", fontsize=15)
+plt.show()
+
+ol_sph = ol_sph.flatten()
+ol_mat = ol_mat.flatten()
+
+plt.hist(ol_sph, bins=bin_bound ,alpha=0.5, density=True, label="Esférica")
+plt.hist(ol_mat, bins=bin_bound,alpha=0.5, density=True,label="Matriz")
+plt.xticks(bin_bound)
+plt.ylabel("Frecuencia", fontsize=15)
+plt.xlabel("Número de círculos tocando el pixel", fontsize=15)
+plt.title("histograma de overlaps por pixel", fontsize=15)
+plt.show()
+
+#kxy = np.linspace(-freq_per_pix * n_samples/2, freq_per_pix * n_samples/2, n_samples)
+#KX, KY = np.meshgrid(kxy, kxy)
+#
+#modk = np.sqrt(KX**2 + KY**2)
+#modk = modk.flatten()
 
 
