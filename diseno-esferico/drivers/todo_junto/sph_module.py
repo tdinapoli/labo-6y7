e7c61c0c0af53a02467a1b924b81d32e8257c63c
soydina <tomas.dina98@gmail.com>
2022-11-28 15:52:21 -0300

    guarado de imagenes en proceso aparte

--- diseno-esferico/drivers/todo_junto/sph_module.py
+++ diseno-esferico/drivers/todo_junto/sph_module.py
@@ -3,7 +3,6 @@ from serial import Serial
 import time as pytime
 import numpy as np
 import threading
-
 BAUDRATE = 115200
 
 def dummy_func():
@@ -52,6 +51,15 @@ class SphericalController(Motor, Illumination):
         if not self.led_pins:
             self.led_pins = {'r':(), 'g':(2, 3, 4, 5, 6, 7, 10), 'b':()} 
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.turn_off_leds()
+        self.close()
+
+    def __del__(self):
+        self.turn_off_leds()
 
     def _open_serial(self) -> Serial:
         """ Opens a serial port between Arduino and Python """
@@ -67,10 +75,12 @@ class SphericalController(Motor, Illumination):
             if pytime.time() - start_time > 5:
                 raise TimeoutError("Arduino is not responding")
         return serial
+
+    def close(self):
+        self._serial.close()
     
     def set_rpm(self, RPM):
         pass
-
 
     def rotate(self, degrees):
  # Actualmente los grados no se corresponden con el giro del motor 
@@ -120,9 +130,9 @@ class SphericalController(Motor, Illumination):
         self._serial.write(f"ON {led_no}\n".encode('ascii'))
         response = self._serial.readline().decode('ascii')
         #print(response)
-        #pytime.sleep(float(time)/1000) # hay que implementar colas para que no sea bloqueante
-        timer = threading.Timer(float(time), dummy_func)
-        timer.start()
+        #pytime.sleep(float(time)/1000) # HAY QUE IMPLEMENTAR COLAS PARA QUE NO SEA BLOQUEANTE
+        #timer = threading.Timer(float(time), dummy_func)
+        #timer.start()
         #self.turn_off_leds()
         return response
 
@@ -130,14 +140,31 @@ class SphericalController(Motor, Illumination):
 if __name__ == "__main__":
     from camera_driver import ImperxCamera 
     import matplotlib.pyplot as plt
+    import multiprocessing as mp
+    from time import perf_counter
+
+    inicio = perf_counter()
+
+
+    def save_img(q, imgs):
+        for i in range(imgs):
+            print(f"on queue: {q.qsize()}")
+            name, barr = q.get()
+            np.save(name, barr)
+
+    mp.set_start_method("fork")
+
     camera = ImperxCamera()
 
     sph = SphericalController("/dev/ttyACM0")
     sph.set_origin()
 
     phis = np.linspace(0.0, 360.0, 40, dtype=float)
+
+    q = mp.Queue(maxsize=50)
+    p = mp.Process(target=save_img, args=(q, len(phis) * len(sph.led_pins["g"]))) # tratar de cambiar para q sea independiente de la cant de fotos
+    p.start()
     for phi in phis:
-        print(phi)
         sph.rotate(phi)
         pytime.sleep(0.5)
         times = np.array([0.2,0.2,0.4,0.4,0.6,0.6,1]) *100
@@ -146,10 +173,15 @@ if __name__ == "__main__":
             sph.turn_on_led(led, "g", time)
             pytime.sleep(0.05) 
             imagen = camera.get_frame()
-            np.save(f'prueba_lentitud/led_{led}_angulo_{phi}.npy', imagen)
+            q.put((f'prueba_lentitud/led_{led}_angulo_{phi}.npy', imagen))
+#
+           # p.join()
+            #np.save(f'prueba_lentitud/led_{led}_angulo_{phi}.npy', imagen)
             #plt.imshow(imagen, cmap = 'gray')
             #plt.show()
 
     sph.rotate(0)
     sph.turn_off_leds()
+    print(perf_counter() - inicio , len(phis) * len(sph.led_pins["g"]) )
+    #camera.close()
             
