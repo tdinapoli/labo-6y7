92185e02a6c1a5d1a21ee779304ae0df5c60f466
soydina <tomas.dina98@gmail.com>
2022-11-28 15:53:57 -0300

    iniciamos analisis de tiempos de exposicion

--- diseno-esferico/drivers/sph_module_cli.py
+++ diseno-esferico/drivers/sph_module_cli.py
@@ -1,4 +1,7 @@
 import sys
+from todo_junto.camera_driver import ImperxCamera
+import numpy as np
+import matplotlib.pyplot as plt
 
 def commands():
     comandos = [
@@ -7,6 +10,9 @@ def commands():
         'l: mover 10 a la izquierda',
         'h: mover 10 a la derecha',
         'a: apagar todos los leds',
+        'i: inicializar camara',
+        's: guardar foto',
+        'c: visualizar foto',
         'q: salir'
     ]
     texto = ''
@@ -39,14 +45,38 @@ if __name__ == "__main__":
 
         elif comando == 'o':
             led = input('seleccionar led ')
-            tiempo = input('Seleccionar tiempo ')
-            resp = sph.turn_on_led(int(led), 'g', int(tiempo))
+            #tiempo = input('Seleccionar tiempo ')
+            resp = sph.turn_on_led(int(led), 'g', 1)
             print(resp)
 
         elif comando == 'a':
             resp = sph.turn_off_leds()
             print(resp)
 
-        else:
+        elif comando == "i":
+            camera = ImperxCamera()
+
+        elif comando == "s":
+            exp = input("Exp time: ")
+            camera.set_gain_exposure(100.0, exp)
+            imagen = camera.get_frame()
+            path = f"/home/chanoscopio/git/labo-6y7/diseno-esferico/exp-time/imagenes/{exp}_oscuridad" 
+            print(path)
+            np.save(path, imagen)
+            print(np.mean(imagen))
+            print('max', np.max(imagen))
+            print('min', np.min(imagen))
+            plt.imshow(imagen, cmap="gray")
+            plt.show()
+
+        elif comando == "c":
+            imagen = camera.get_frame()
+            print(np.mean(imagen))
+            print('max', np.max(imagen))
+            print('min', np.min(imagen))
+            plt.imshow(imagen, cmap="gray")
+            plt.show()
+
+        elif comando != 'q':
             print('comando incorrecto')
 
